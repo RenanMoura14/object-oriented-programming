7402c91fad8636b60eafc23719e5fd42f04af27a
RenanMoura14 <renan.moura.silva.06@aluno.ifce.edu.br>
2020-10-05 21:59:57 -0300

    calcular salario

--- heranca/src/QuestionTwo/FuncionarioComissionado.java
+++ heranca/src/QuestionTwo/FuncionarioComissionado.java
@@ -3,9 +3,14 @@ package QuestionTwo;
 public class FuncionarioComissionado extends Funcionario {
 	double comissaoVenda;
 
-	public FuncionarioComissionado(String primeiroNome, String segundoNome, double salario) {
+	public FuncionarioComissionado(String primeiroNome, String segundoNome, double salario, double comissaoVenda) {
 		super(primeiroNome, segundoNome, salario);
+		this.comissaoVenda = comissaoVenda;
 		// TODO Auto-generated constructor stub
 	}
+	
+	 double calcularSalario(){
+		 return comissaoVenda + getSalario();
+	 } 
 		
 }
