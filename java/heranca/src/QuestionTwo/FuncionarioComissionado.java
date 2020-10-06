6ceefe54061de09821109fb65e2be6487d357d68
RenanMoura14 <renan.moura.silva.06@aluno.ifce.edu.br>
2020-10-06 14:48:05 -0300

    comentarios sobre super e extends

--- java/heranca/src/QuestionTwo/FuncionarioComissionado.java
+++ java/heranca/src/QuestionTwo/FuncionarioComissionado.java
@@ -1,12 +1,14 @@
 package QuestionTwo;
 
 public class FuncionarioComissionado extends Funcionario {
+	//exetends palavra reservada para herdar os métodos e atributos de determinada classe
 	double comissaoVenda;
 
 	public FuncionarioComissionado(String primeiroNome, String segundoNome, double salario, double comissaoVenda) {
+		//super palavra reservada para passar os dados para super classe
 		super(primeiroNome, segundoNome, salario);
+		
 		this.comissaoVenda = comissaoVenda;
-		// TODO Auto-generated constructor stub
 	}
 	
 	 double calcularSalario(){
