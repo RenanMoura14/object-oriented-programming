71b1f399a3ca2d8f8504128074ebc826b3ce4d96
RenanMoura14 <renan.moura.silva.06@aluno.ifce.edu.br>
2020-10-05 21:14:06 -0300

    iniciando classes da atividade

---
+++ heranca/src/QuestionTwo/FuncionarioComissionado.java
@@ -0,0 +1,11 @@
+package QuestionTwo;
+
+public class FuncionarioComissionado extends Funcionario {
+
+	public FuncionarioComissionado(String primeiroNome, String segundoNome, double salario) {
+		super(primeiroNome, segundoNome, salario);
+		// TODO Auto-generated constructor stub
+	}
+	
+		
+}
